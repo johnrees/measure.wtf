79f000693841688643aab98c1c4d4fecbf172d06
John Rees <john@bitsushi.com>
2020-05-30 18:40:41 +0100

    Updates

---
+++ precache-manifest.c20cda3d5c5c6063cabb3cc82419cb7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e983ab371fbd6497472496a0619fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f509b69c8a5a2a77e4f2",
+    "url": "/static/css/main.33d68060.chunk.css"
+  },
+  {
+    "revision": "65ee4e1b8179ad76b9b1",
+    "url": "/static/js/2.2c16cede.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c16cede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f509b69c8a5a2a77e4f2",
+    "url": "/static/js/main.42bf2026.chunk.js"
+  },
+  {
+    "revision": "e0cd1065705487935c7b",
+    "url": "/static/js/runtime-main.7bc6ed9f.js"
+  }
+]);
